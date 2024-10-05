a8338b7450a028df711f3777c857da219b765e3c
Minindu-Fernando <maheshdelpitiya@gmail.com>
2024-10-05 19:44:26 +0530

    dash board added.

---
+++ Day03-Maven/src/main/java/controller/dashboard/DashBoardFormController.java
@@ -0,0 +1,50 @@
+package controller.dashboard;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class DashBoardFormController {
+
+    @FXML
+    private JFXButton btnCustomerForm;
+
+    @FXML
+    private JFXButton btnItemform;
+
+    @FXML
+    private JFXButton btnPlaceOrderForm;
+
+    @FXML
+    void btnCustomerFormOnAction(ActionEvent event) {
+        Stage stage = new Stage();
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/customer_form.fxml"))));
+            stage.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @FXML
+    void btnItemFormOnAction(ActionEvent event) {
+        Stage stage = new Stage();
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/item_form.fxml"))));
+            stage.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @FXML
+    void btnPlaceOrderFormOnAction(ActionEvent event) {
+
+    }
+
+}
