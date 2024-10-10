a01822c73627694a03954131f0fe340e2b3fbfea
Minindu-Fernando <maheshdelpitiya@gmail.com>
2024-10-10 19:55:46 +0530

    order detail added

--- Day03-Maven/src/main/java/controller/order/OrderController.java
+++ Day03-Maven/src/main/java/controller/order/OrderController.java
@@ -1,9 +1,27 @@
 package controller.order;
 
+import db.DBConnection;
 import model.Order;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class OrderController {
-    public Boolean placeOrder(Order order){
+    public Boolean placeOrder(Order order) throws SQLException {
+        Connection connection = DBConnection.getInstance().getConnection();
+        PreparedStatement psTm = connection.prepareStatement("INSERT INTO Orders VALUE(?,?,?)");
+        psTm.setObject(1,order.getOrderId());
+        psTm.setObject(2,order.getOrderDate());
+        psTm.setObject(3,order.getCustomerId());
+
+        boolean isOrderAdd = psTm.executeUpdate() > 0;
+        if(isOrderAdd){
+            boolean isOrderDetailAdd = OrderDetailController.addOrderDetail(order.getOrderDetails());
+            if (isOrderDetailAdd){
+
+            }
+        }
         return true;
     }
 }
---
+++ Day03-Maven/src/main/java/controller/order/OrderDetailController.java
@@ -0,0 +1,24 @@
+package controller.order;
+import model.OrderDetail;
+import util.CrudUtil;
+import java.sql.SQLException;
+import java.util.List;
+
+public class OrderDetailController {
+    public static boolean addOrderDetail(List<OrderDetail> orderDetails) throws SQLException {
+        for (OrderDetail orderDetail : orderDetails){
+            boolean isOrderDetailAdd = addOrderDetail(orderDetail);
+            if (!isOrderDetailAdd){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static boolean addOrderDetail(OrderDetail orderDetail) throws SQLException {
+        return CrudUtil.execute("INSERT INTO orderdetil VALUES(?,?,?,?)",
+                orderDetail.getOrderId(),
+                orderDetail.getItemCode(),
+                orderDetail.getQty(),
+                orderDetail.getDiscount()
+        );
+}
